f7e55af53f8ccf90b690695146021eb1923f4da9
Nate Willard <nate.a.willard@gmail.com>
2016-02-02 14:27:31 -0800

    Update to use tkinter to control gpio

--- LED.py
+++ LED.py
@@ -1,13 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+import Tkinter as tk
+
+def onKeyPress(event):
+    text.insert('end', 'You pressed %s\n' % (event.char, ))
+    GPIO.output(led, GPIO.HIGH)
+
+def onKeyRelease(event):
+    text.insert('end', 'You released %s\n' % (event.char, ))
+    GPIO.output(led, GPIO.LOW)
+
 GPIO.setmode(GPIO.BCM)
 led = 14
 GPIO.setup(led, GPIO.OUT)
-while True:
-	print 'LED on'
-	GPIO.output(led, GPIO.HIGH)
-	time.sleep(1)
-	print 'LED off'
-	GPIO.output(led, GPIO.LOW)
-	time.sleep(1)
+
+# while True:
+# 	print 'LED on'
+# 	GPIO.output(led, GPIO.HIGH)
+# 	time.sleep(1)
+# 	print 'LED off'
+# 	GPIO.output(led, GPIO.LOW)
+# 	time.sleep(1)
+
 GPIO.cleanup()
+
+
+root = tk.Tk()
+root.geometry('300x200')
+text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+text.pack()
+root.bind('<KeyPress>', onKeyPress)
+root.bind('<KeyRelease>', onKeyRelease)
+root.mainloop()
