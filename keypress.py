5912b9fb6ab9d7229c9f1130741ab27e69a3106c
Nate Willard <nate.a.willard@gmail.com>
2016-02-02 14:25:17 -0800

    Update tkinter keypress handling

--- keypress.py
+++ keypress.py
@@ -3,9 +3,13 @@ import Tkinter as tk
 def onKeyPress(event):
     text.insert('end', 'You pressed %s\n' % (event.char, ))
 
+def onKeyRelease(event):
+    text.insert('end', 'You released %s\n' % (event.char, ))
+
 root = tk.Tk()
 root.geometry('300x200')
 text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
 text.pack()
 root.bind('<KeyPress>', onKeyPress)
+root.bind('<KeyRelease>', onKeyRelease)
 root.mainloop()
