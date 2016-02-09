d7aeaa8ce9e8005fbac4740e24e0699ce9ab256a
Nate Willard <nate.a.willard@gmail.com>
2016-02-09 11:30:45 -0800

    Add basic pi server connectivity

--- javaServer/piServer.py
+++ javaServer/piServer.py
@@ -19,6 +19,24 @@ except socket.error as msg:
      
 print 'Socket bind complete'
 
+s.listen(10)
+print 'Socket now listening'
+
+def clientthread(conn):
+    print 'Driver connected'
+
+def listenForConnections():
+    while 1:
+        #wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+        #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+        start_new_thread(clientthread ,(conn,))
+
+start_new_thread(listenForConnections, ())
+s.close()
+
 # GPIO.output(led, GPIO.HIGH)
 
 
